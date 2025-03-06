a27c6991eefa254332a253bf543ca1013ed81cc9
Ian <iankruze60@gmail.com>
2025-03-06 21:48:05 +0300

    Adds modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate", batheDog = false) {
+    let result = `This Saturday, I want to ${activity}!`;
+
+    if (activity === "roller-skate" && batheDog) {
+        result = "This Saturday, I want to bathe my dog!";
+    }
+
+    return result;
+}
+
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
